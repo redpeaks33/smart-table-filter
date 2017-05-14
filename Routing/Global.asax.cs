6fc5949a27e67dffc0ff23335194fcb8ab73723d
redpeaks33 <redpeaks33@gmail.com>
2017-05-14 14:08:35 +0900

    smart table filter in angularjs
    initialize

---
+++ Routing/Global.asax.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.WebHost;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace Routing
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public class MyHttpControllerRouteHandler : HttpControllerRouteHandler
+        {
+            protected override IHttpHandler GetHttpHandler(RequestContext requestContext)
+            {
+                return new MyHttpControllerHandler(requestContext.RouteData);
+            }
+        }
+
+        public class MyHttpControllerHandler : HttpControllerHandler, IRequiresSessionState
+        {
+            public MyHttpControllerHandler(RouteData routeData)
+                : base(routeData)
+            {
+            }
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var route = routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+            );
+            route.RouteHandler = new MyHttpControllerRouteHandler();
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Routing/js/controller.js
@@ -0,0 +1,144 @@
+﻿var main = angular.module("app", ['ui.router']);
+
+main.controller('MyController', ['$scope', '$http', function ($scope, $http) {
+    $scope.Message = 'Click Button';
+
+    var
+       nameList = [{ id: 1, name: 'Pierre' }, { id: 2, name: 'Pol' }, {id:3,name:'Jacques'}, {id:4,name:'Robert'}, {id:5,name:'Elisa'}],
+       familyName = [{id:1,name:'Dupont'}, {id:2,name:'Germain'}, {id:3,name:'Delcourt'}, {id:4,name:'bjip'}, {id:5,name:'Menez'}],
+       nationList = [{id:1,name:'USA'}, {id:2,name:'France'}, {id:3,name:'Germany'}],
+       educationList = [{ id: 1, name: 'Doctorate' }, { id: 2, name: 'Master' }, { id: 3, name: 'Bachelor' }, { id: 4, name: 'High school' }];
+
+    function createRandomItem() {
+        var
+          firstName = nameList[Math.floor(Math.random() * 5)],
+          lastName = familyName[Math.floor(Math.random() * 5)],
+          nationality = nationList[Math.floor(Math.random() * 3)],
+          education = educationList[Math.floor(Math.random() * 4)];
+
+        return {
+            firstName: firstName,
+            lastName: lastName,
+            nationality: nationality,
+            education: education
+        };
+    }
+
+    $scope.itemsByPage = 15;
+
+    $scope.collection = [];
+    
+    for (var j = 0; j < 200; j++) {
+        $scope.collection.push(createRandomItem());
+    };
+    $scope.displayed = [].concat($scope.collection);
+
+
+}]);
+
+main.directive('tableFilter', function () {
+    return {
+        restrict: 'EA',
+        //require: '^table',
+        scope: {
+            original:'=',
+            collection: '='
+        },
+        controller: ['$scope', function ($scope) {
+            $scope.filterInfoContainer = {};
+
+            this.getFilterInfoContainer = function () {
+                return $scope.filterInfoContainer;
+            };
+
+            this.execute = function (element) {
+                let filteredCollection = [];
+
+                //if selected all, don't execute filter. Display all
+                if (element.value == 'All')
+                {
+                    //Synchronize with 'All'
+                    _.each($scope.original, function (item) {
+                        item.visible = element.selected;
+                    })
+
+                    _.each($scope.filterInfoContainer, function (filterInfo, key) {
+                        var filterList = _.each(filterInfo, function (n) {
+                            n.selected = element.selected;
+                        });
+                    });
+                }
+                else {
+                
+                //initialize
+                _.each($scope.original, function (item) {
+                        item.visible = true;
+                })
+
+                //filter 
+                _.each($scope.filterInfoContainer, function (filterInfo,key) {
+                    var filterList = _.where(filterInfo, { selected: false })
+
+                    _.each($scope.original, function (item) {
+                        if (item.visible && _.filter(filterList, function (n) { return n.value == item[key].name }).length > 0)
+                        {
+                            item.visible = false;
+                        }
+                    })
+                });
+                }
+                //set collection in order to display
+                $scope.collection = _.filter($scope.original, function (n) { return n.visible })
+            };
+        }],
+        link: function (scope, element, attr, ctrl) {
+            scope.filterInfoContainer = {};
+        }
+    };
+});
+
+main.directive('tableColumnFilter', function () {
+    return {
+        restrict: 'EA',
+        require: '^tableFilter',
+        scope: {
+            collection: '=',
+                predicate: '@',
+        title: '@'
+        },
+        templateUrl: 'TableColumnFilter.html',
+        link: function (scope, element, attr, tableFilterCtrl) {
+            initialize();
+            function initialize() {
+                //add title.
+                scope.dropdownLabel = scope.title;
+                //extract predicate and get uniq and sort.
+                let converted = _.sortBy(_.uniq(_.flatten(_.pluck(scope.collection, scope.predicate))));
+                //Add 'All' to list head.
+                converted.unshift({
+                    name: 'All',
+                    selected: true
+                });
+                //convert for checkbox item list.
+                scope.distinctItems = _.map(converted, function (n) {
+                    let item = {
+                        value: n.name,
+                        selected: true
+                    }
+
+                    return item;
+                });
+
+                tableFilterCtrl.getFilterInfoContainer()[scope.predicate] = scope.distinctItems;
+            }
+
+            scope.filterChanged = function (element) {
+                tableFilterCtrl.getFilterInfoContainer()[scope.predicate] = scope.distinctItems;
+                tableFilterCtrl.execute(element);
+            }
+        }
+
+    };
+});
+
+
