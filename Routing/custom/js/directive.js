deea19475f9b388826efe12eea40a89bbb8e36b4
redpeaks33 <redpeaks33@gmail.com>
2017-06-03 18:46:57 +0900

    add

--- Routing/custom/js/directive.js
+++ Routing/custom/js/directive.js
@@ -3,21 +3,20 @@
         restrict: 'EA',
         //require: '^table',
         scope: {
-            original:'=',
+            original: '=',
             collection: '='
         },
-        controller: ['$scope','$rootScope','$timeout', function ($scope,$rootScope,$timeout) {
+        controller: ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
             $scope.filterInfoContainer = {};
             $scope.isFiltering = false;
             this.getFilterInfoContainer = function () {
                 return $scope.filterInfoContainer;
             };
 
-            this.execute = function (element,predicate) {
+            this.execute = function (element, predicate) {
                 $scope.isFiltering = true;
                 //if selected all, don't execute filter. Display all
-                if (element.value == 'All')
-                {
+                if (element.value == 'All') {
                     //Synchronize with 'All'
                     _.each($scope.original, function (item) {
                         item.visible = element.selected;
@@ -66,17 +65,17 @@
                     //    //})
                     //});
                     //$rootScope.$broadcast('updateFilterInfo', $scope.filterInfoContainer)
-                //_.each($scope.filterInfoContainer, function (filterInfo, key) {
-                //    var filterInfoList = _.filter(filterInfo, function (n) { return n == element });
-                //    if (filterInfoList.length == 0) {
-                //        var filterList = _.each(filterInfo, function (n) {
-                //            n.selected = true;
-                //        });
-                //    }
-                //    else {
-                //        _.filter(filterInfo, function (n) { return n.value == 'All' })[0].selected = true;
-                //    }
-                //});
+                    //_.each($scope.filterInfoContainer, function (filterInfo, key) {
+                    //    var filterInfoList = _.filter(filterInfo, function (n) { return n == element });
+                    //    if (filterInfoList.length == 0) {
+                    //        var filterList = _.each(filterInfo, function (n) {
+                    //            n.selected = true;
+                    //        });
+                    //    }
+                    //    else {
+                    //        _.filter(filterInfo, function (n) { return n.value == 'All' })[0].selected = true;
+                    //    }
+                    //});
                 }
                 //set collection in order to display
                 $scope.collection = _.filter($scope.original, function (n) { return n.visible })
@@ -94,6 +93,8 @@
 });
 
 main.directive('tableColumnFilter', function () {
+
+
     return {
         restrict: 'EA',
         require: '^tableFilter',
@@ -144,52 +145,212 @@ main.directive('tableColumnFilter', function () {
     };
 });
 
-main.directive('tableColumnSort', function () {
+main.directive('filRow', function () {
     return {
         restrict: 'EA',
-        require: '?^table',
+        //require: '^table',
         scope: {
-            collection: '=',
+            original: '=',
+            showing: '='
+        },
+        controller: ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
+            $scope.filterInfoContainer = {};
+
+            //#region getter
+            this.getFilterInfoContainer = function () {
+                return $scope.filterInfoContainer;
+            }
+
+            this.getOriginalCollection = function () {
+                return $scope.original;
+            };
+
+            this.getShowingCollection = function () {
+                return $scope.showing;
+            };
+            //#endregion
+
+            //#region execute filter
+            this.executeFilter = function (element, predicate) {
+                //if selected all, don't execute filter. Display all
+                if (element.value == 'All') {
+                    //Synchronize with 'All'
+                    _.each($scope.original, function (item) {
+                        item.visible = element.selected;
+                    })
+
+                    _.each($scope.filterInfoContainer, function (filterInfo, key) {
+                        _.each(filterInfo, function (n) {
+                            n.selected = element.selected;
+                        });
+                    });
+                }
+                else {
+                    //initialize
+                    _.each($scope.original, function (item) {
+                        item.visible = true;
+                    });
+
+                    _.each($scope.filterInfoContainer, function (filterInfo, key) {
+                        _.each(filterInfo, function (n) {
+                            n.selected = element.selected;
+                        });
+                    });
+
+                    //_.find($scope.filterInfoContainer[predicate], { value: 'All' }).selected = true;
+                    //_.each($scope.filterInfoContainer[predicate], function (filterInfo) {
+                    //    _.find(filterInfo, { value: 'All' }).selected = true;
+                    //});
+
+                    //filter original list 
+                    _.each($scope.filterInfoContainer, function (filterInfo, key) {
+                        var filterList = _.where(filterInfo, { selected: false })
+                        _.each($scope.original, function (item) {
+                            if (item.visible && _.filter(filterList, function (n) { return n.value == item[key].name }).length > 0) {
+                                item.visible = false;
+                            }
+                        })
+                    });
+
+                    //create filterInfoContainer from filtered original list; !!!
+                    //_.each($scope.filterInfoContainer, function (filterInfo, key) {
+                    //    let converted = _.sortBy(_.uniq(_.flatten(_.pluck(scope.collection, key))));
+                    //    //_.each($scope.original, function (item) {
+                    //    //    if (item.visible && _.filter(filterList, function (n) { return n.value == item[key].name }).length > 0) {
+                    //    //        item.visible = false;
+                    //    //    }
+                    //    //})
+                    //});
+                    //$rootScope.$broadcast('updateFilterInfo', $scope.filterInfoContainer)
+                    //_.each($scope.filterInfoContainer, function (filterInfo, key) {
+                    //    var filterInfoList = _.filter(filterInfo, function (n) { return n == element });
+                    //    if (filterInfoList.length == 0) {
+                    //        var filterList = _.each(filterInfo, function (n) {
+                    //            n.selected = true;
+                    //        });
+                    //    }
+                    //    else {
+                    //        _.filter(filterInfo, function (n) { return n.value == 'All' })[0].selected = true;
+                    //    }
+                    //});
+                }
+                //set collection in order to display
+                $scope.showing = _.filter($scope.original, function (n) { return n.visible })
+            };
+            //#endregion
+
+            //#region sort
+            this.executeSort = function (isDesc, predicate) {
+                $scope.showing = _.sortBy($scope.showing, function (n) {
+                    return n[predicate].name;
+                });
+                if (isDesc) {
+                    $scope.showing = $scope.showing.reverse();
+                }
+            };
+            //#endregion
+
+            //#region text search 
+            this.executeTextSearch = function (text,predicate) {
+                //set collection in order to display
+                $scope.showing = _.filter($scope.showing, function (n) {
+                    return n[predicate].name.toLowerCase().indexOf(text) != -1;
+                })
+            };
+            //#endregion
+
+            //#region initialize
+            this.executeInitialize = function () {
+                //set collection in order to display
+                $scope.showing = $scope.original;
+            };
+            //#endregion
+        }],
+        link: function (scope, element, attr, ctrl) {
+            scope.filterInfoContainer = {};
+            scope.$parent.isFiltering = false;
+        }
+    };
+});
+
+main.directive('filCol', function () {
+    return {
+        restrict: 'EA',
+        require: '^filRow',
+        scope: {
             predicate: '@',
             title: '@',
+            onlyshowing: '=',
+            onlychecked: '='
         },
-        templateUrl: '/custom/html/TableColumnSort.html',
+        templateUrl: '/custom/html/TableColumnFilter.html',
         link: function (scope, element, attr, tableFilterCtrl) {
-            var stateInfo = [{ id: 0, name: 'normal' }, { id: 1, name: 'asc' }, { id: 2, name: 'desc' }];
-            scope.state = stateInfo[0];
-            scope.execute = function () {
-                scope.state = stateInfo[++scope.state.id % 2];
+            initialize();
 
-                var sorted = _.sortBy(scope.collection, function (n) {
-                    return n[scope.predicate].name;
+            function initialize() {
+                //add title.
+                scope.dropdownLabel = scope.title;
+
+                //initialize collection
+                scope.original = tableFilterCtrl.getOriginalCollection();
+                scope.showing = tableFilterCtrl.getShowingCollection();
+                scope.distinctItems = createDistinctItems();
+
+                //set this column state to parent filter container .
+                updateParentFilterContainer();
+            }
+
+            function createDistinctItems() {
+                //extract predicate and get uniq and sort.
+                let converted = _.sortBy(_.uniq(_.flatten(_.pluck(scope.original, scope.predicate))));
+                //Add 'All' to list head.
+                converted.unshift({
+                    name: 'Select All',
+                    selected: true
                 });
-                if(scope.state.id == 2){
-                    scope.collection = sorted.reverse();
-                }
-                else if (scope.state.id == 1) {
-                     scope.collection  = sorted;
-                }
+                //convert for checkbox item list.
+                return _.map(converted, function (n) {
+                    let item = {
+                        value: n.name,
+                        selected: true
+                    }
+                    return item;
+                });
+            }
+
+            function updateParentFilterContainer() {
+                tableFilterCtrl.getFilterInfoContainer()[scope.predicate] = scope.distinctItems;
+            }
+
+            scope.filterChanged = function (element) {
+                updateParentFilterContainer();
+                tableFilterCtrl.executeFilter(element, scope.predicate);
             };
+
+            scope.sort = function (isDesc) {
+                tableFilterCtrl.executeSort(isDesc, scope.predicate);
+            };
+
+            scope.clear = function () {
+                tableFilterCtrl.executeInitialize();
+            };
+
+            scope.textSearch = function (text) {
+                tableFilterCtrl.executeTextSearch(text, scope.predicate);
+            };
+
+            scope.showListingItem = function () {
+                updateParentFilterContainer();
+            };
+
+            scope.showCheckedItem = function () {
+                //
+                updateParentFilterContainer();
+             };
+
+            scope.$on('updateFilterInfo', function (event, filterInfoContainer) {
+                tableFilterCtrl.getFilterInfoContainer()[scope.predicate] = filterInfoContainer[scope.predicate]
+            });
         },
     };
 });
-
-main.directive('tableSearch', function () {
-    return {
-        restrict: 'EA',
-        require: '?^table',
-        scope: {
-            collection: '=',
-        },
-        templateUrl: '/custom/html/TableSearch.html',
-        link: function (scope, element, attr, tableFilterCtrl) {
-            scope.execute = function (model) {
-                _.each(this.collection, function (n) {
-                    alert(n);
-                });
-            };
-        },
-    };
-});
-
-
