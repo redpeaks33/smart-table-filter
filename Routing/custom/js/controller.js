fc110967259bdc4cd484ed3d1ae9f679ee4bbfa9
redpeaks33 <redpeaks33@gmail.com>
2017-06-25 22:08:18 +0900

    refactoring

--- Routing/custom/js/controller.js
+++ Routing/custom/js/controller.js
@@ -1,13 +1,24 @@
 ﻿var main = angular.module("app", []);
 
-main.controller('MyController', ['$scope', '$http', function ($scope, $http) {
-    $scope.Message = 'Click Button';
+main.controller('MyController', ['$scope',  function ($scope) {
+    $scope.collection = [];
 
+    $scope.initialize = function()
+    {
+        //createRandomList();
+        createFixedList();
+    }
+    //#region Random List
+    function createRandomList() {
+        for (var j = 0; j < 100; j++) {
+            $scope.collection.push(createRandomItem(j));
+        };
+    }
     var
-       nameList = [{ id: 1, name: 'Pierre' }, { id: 2, name: 'Pol' }, {id:3,name:'Jacques'}, {id:4,name:'Robert'}, {id:5,name:'Elisa'}],
-       familyName = [{id:1,name:'Dupont'}, {id:2,name:'Germain'}, {id:3,name:'Delcourt'}, {id:4,name:'bjip'}, {id:5,name:'Menez'}],
-       nationList = [{id:1,name:'USA'}, {id:2,name:'France'}, {id:3,name:'Germany'}],
-       educationList = [{ id: 1, name: 'Doctorate' }, { id: 2, name: 'Master' }, { id: 3, name: 'Bachelor' }, { id: 4, name: 'High school' }];
+        nameList = [{ id: 1, name: 'Pierre' }, { id: 2, name: 'Pol' }, { id: 3, name: 'Jacques' }, { id: 4, name: 'Robert' }, { id: 5, name: 'Elisa' }],
+        familyName = [{ id: 1, name: 'Dupont' }, { id: 2, name: 'Germain' }, { id: 3, name: 'Delcourt' }, { id: 4, name: 'bjip' }, { id: 5, name: 'Menez' }],
+        nationList = [{ id: 1, name: 'USA' }, { id: 2, name: 'France' }, { id: 3, name: 'Germany' }],
+        educationList = [{ id: 1, name: 'Doctorate' }, { id: 2, name: 'Master' }, { id: 3, name: 'Bachelor' }, { id: 4, name: 'High school' }];
 
     function createRandomItem(index) {
         var firstName = nameList[Math.floor(Math.random() * 5)],
@@ -16,33 +27,21 @@ main.controller('MyController', ['$scope', '$http', function ($scope, $http) {
           education = educationList[Math.floor(Math.random() * 4)];
 
         return {
-            id:index,
+            id: index,
             firstName: firstName,
             lastName: lastName,
             nationality: nationality,
             education: education
         };
     }
+    //#endregion Random List
 
-    $scope.collection = [];
-
-    initialize();
-
-    function initialize() {
-        //createRandomList();
-        createFixedList();
-    }
-
-    function createRandomList() {
-        for (var j = 0; j < 100; j++) {
-            $scope.collection.push(createRandomItem(j));
-        };
-    }
+    //#region Fixed List
     function createFixedList ()
     {
         var line = [{id:1,name:11},{id:2,name:12},{id:3,name:13},{id:4,name:14},{id:5,name:15}]
         var model = [{id:1,name:'A'},{id:2,name:'B'},{id:3,name:'C'},{id:4,name:'D'},{id:5,name:'E'}]
-        for (var j = 0; j < 100; j++) {
+        for (var j = 0; j < 10; j++) {
             var item = {
                 id: j,
                 firstName: line[Math.floor((Math.random() * 5))],
@@ -53,5 +52,6 @@ main.controller('MyController', ['$scope', '$http', function ($scope, $http) {
             $scope.collection.push(item);
         };
     }
+    //#endregion Fixed List
 
 }]);
